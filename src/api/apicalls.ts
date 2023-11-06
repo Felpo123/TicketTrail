09757a115c6a412b376eef2d7d95a1ae044fda01
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2023-11-06 08:19:55 -0300

    feat: movie details screen, top cast, and more

--- src/api/apicalls.ts
+++ src/api/apicalls.ts
@@ -2,15 +2,15 @@ const apikey: string = 'cc311cf7965c68c2d7b9cec0439f090d'
 export const baseImagePath = (size: string, path: string) => {
     return `https://image.tmdb.org/t/p/${size}${path}`;
 };
-export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`
-export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}`
-export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`
+export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=es&region=CL`
+export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}&language=es&region=CL`
+export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=es&region=CL`
 export const searchMovies = (keyword:string) => {
-    return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}`
+    return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}&language=es&region=CL`
 }
 export const movieDetails = (id: number) => {
-    return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`;
+    return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}&language=es`;
 };
 export const movieCastDetails = (id: number) => {
-    return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}`;
+    return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}&language=es`;
 };
